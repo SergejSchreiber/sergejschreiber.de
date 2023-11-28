1759d54137a2eb9327ce40c1bac3bd1b211932c2
Sergej Schreiber <132712491+SergejSchreiber@users.noreply.github.com>
2023-11-28 21:15:18 +0100

    update

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.scss'
 })
-export class MenuComponent {
+export class MenuComponent implements OnInit {
+  @Input() darkMode = true;
+  constructor() { }
 
+  ngOnInit(): void {
+  }
 }
