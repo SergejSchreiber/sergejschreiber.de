936c22ded281cd63f2bd205cd21c52a2c7de64e3
Sergej Schreiber <132712491+SergejSchreiber@users.noreply.github.com>
2023-11-28 16:26:22 +0100

    added images and started animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'sergejschreiber';
+export class AppComponent implements OnInit {
+  images = ['1.jpg', '2.jpg', '3.jpg'];
+  currentImage = 0;
+  showImage = true;
+
+  ngOnInit() {
+    this.updateImage();
+  }
+
+  updateImage() {
+    setInterval(() => {
+      this.currentImage++;
+      this.currentImage = this.currentImage % this.images.length;
+      this.showImage = false;
+
+      setTimeout(() => {
+        this.showImage = true;
+      },5);
+    }, 8000);
+  }
+
+
 }
